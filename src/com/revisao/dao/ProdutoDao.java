fd31cb7b0b5174498961b098b80faa7dbd6e4130
Suzyanne Oliveira <suzy_0310@yahoo.com.br>
2014-10-27 14:39:57 -0200

    Implementação Dao e Model do Produto

---
+++ src/com/revisao/dao/ProdutoDao.java
@@ -0,0 +1,86 @@
+package com.revisao.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import com.revisao.model.ProdutoModel;
+import com.revisao.util.Conexao;
+
+public class ProdutoDao {
+	private Connection connection;
+
+    public ProdutoDao() {
+        connection = Conexao.getConnection();
+    }
+    
+    public void addProduto(ProdutoModel produto) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("insert into produtos(produtoid,produtonome,descricao) values (?, ?, ? )");
+
+            preparedStatement.setInt(1, produto.getProdutoid());
+            preparedStatement.setString(2, produto.getProdutoNome());
+            preparedStatement.setString(3, produto.getDescricao());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void deleteProduto(int produtoid) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("delete from produtos where produtoid=?");
+            // Parameters start with 1
+            preparedStatement.setInt(1, produtoid);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void updateProduto(ProdutoModel produto) {
+        try {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("update produtos set produtoid=?, produtonome=?, descricao=?" +
+                            "where produtoid=?");
+      
+            preparedStatement.setInt(1, produto.getProdutoid());
+            preparedStatement.setString(2, produto.getProdutoNome());
+            preparedStatement.setString(3, produto.getDescricao());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public List<ProdutoModel> getAllProduto() {
+        List<ProdutoModel> produtos = new ArrayList<ProdutoModel>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from produtos");
+            while (rs.next()) {
+            	ProdutoModel produto = new ProdutoModel();
+            	produto.setProdutoid(rs.getInt("produtoid"));
+            	produto.setProdutoNome(rs.getString("produtonome"));
+            	produto.setDescricao(rs.getString("descricao"));
+   
+            	produtos.add(produto);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return produtos;
+    }
+
+}
---
+++ src/com/revisao/model/ProdutoModel.java
@@ -0,0 +1,33 @@
+package com.revisao.model;
+
+public class ProdutoModel {
+
+    private int produtoid;
+    private String descricao,produtonome;
+
+    
+    public int getProdutoid() {
+        return produtoid;
+    }
+    public void setProdutoid(int produtoid) {
+        this.produtoid = produtoid;
+    }
+    
+    public String getProdutoNome() {
+        return produtonome;
+    }
+    public void setProdutoNome(String produtonome) {
+        this.produtonome = produtonome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        return "User [produtoid=" + produtoid + ", descricao=" + descricao+ "]";
+    }    
+}
